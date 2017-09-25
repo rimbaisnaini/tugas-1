77e5c71a41a67bb901a8d2a5e24d1856004558f2
rimbaisnaini <32252807+rimbaisnaini@users.noreply.github.com>
2017-09-25 10:03:12 +0700

    Add files via upload

---
+++ tugas1-04317025 _cherry.py
@@ -0,0 +1,9 @@
+nama = "Rimba Isnaini Cherry Ningsih"
+nim = "04317025"
+umur = 20
+print ("namaku:" + nama)
+print ("noimku :"+ nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5 )
